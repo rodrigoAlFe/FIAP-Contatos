729d9cbf7499a50355a23ffa32c9357362e57604
Carlos Alberto Filho <79123311+carlosalbertofilho@users.noreply.github.com>
2025-01-09 13:31:15 -0300

    Add initial migration and database schema setup
    This commit introduces the first migration for the database, creating the "Contatos" table with columns for contact details such as name, email, phone, and timestamps. A unique index is added to the Email column. Supporting files for EF Core and database integration are also included.

---
+++ src/FIAP.Infrastructure/Migrations/20250109003737_V1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FIAP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    DDD = table.Column<string>(type: "varchar(2)", maxLength: 2, nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime", nullable: false),
+                    AtualizadoEm = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contatos_Email",
+                table: "Contatos",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contatos");
+        }
+    }
+}
