6acfb073252ede7d29a2139420052c1ee6724032
Carlos Alberto Filho <79123311+carlosalbertofilho@users.noreply.github.com>
2025-01-08 21:38:22 -0300

    Add SQL Server support, Swagger setup, and service installer
    Added SQL Server container configuration in `compose.yaml` and connection string to `appsettings.json`. Integrated Swagger through a new `SwaggerServiceInstaller` class for API documentation. Refactored `Program.cs` to utilize service installation for better modularity and maintainability.

---
+++ src/FIAP.CrossCutting/Configuration/SwaggerServiceInstaller.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace FIAP.CrossCutting.Configuration;
+
+public class SwaggerServiceInstaller : IServiceInstaller
+{
+    public void SetupServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer(); 
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "FIAP API",
+                Version = "v1",
+                Description = "API para gerenciar os recursos do FIAP."
+            });
+        });
+    }
+}
