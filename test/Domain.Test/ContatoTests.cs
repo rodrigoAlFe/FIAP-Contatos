f936cd758b0159374ee0497d7e233328f39d320e
Carlos Alberto Filho <79123311+carlosalbertofilho@users.noreply.github.com>
2025-01-10 19:18:28 -0300

    Add unit tests for Contato entity
    Implemented `ContatoTests` to validate the correctness of the `Contato` entity, including scenarios for valid and invalid field values. Added a new test project `Domain.Test` and included it in the solution configuration. This ensures proper coverage and validation for domain model constraints.

---
+++ test/Domain.Test/ContatoTests.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FIAP.Contatos.Domain.Entities;
+
+namespace Domain.Test;
+
+public class ContatoTests
+{
+    [Fact]
+    public void Contato_DeveSerValido_QuandoTodosOsCamposForemPreenchidosCorretamente()
+    {
+        // Arrange
+        var contato = new Contato
+        {
+            Id = 1,
+            Nome = "João Silva",
+            Telefone = "1234-5678",
+            Email = "joao@exemplo.com",
+            Ddd = 11
+        };
+
+        // Act
+        var validationResults = ValidateModel(contato);
+
+        // Assert
+        Assert.Empty(validationResults); // Sem erros de validação
+    }
+
+    [Theory]
+    [InlineData(null, "1234-5678", "joao@exemplo.com", 11, "O Campo Nome é obrigatório.")]
+    [InlineData("João Silva", "abc1234", "joao@exemplo.com", 11, "Telefone no formato inválido.")]
+    [InlineData("João Silva", "1234-5678", "email_invalido", 11, "Endereço de e-mail inválido.")]
+    [InlineData("João Silva", "1234-5678", "joao@exemplo.com", 123, "DDD no formato inválido.")]
+    public void Contato_DeveSerInvalido_ComDadosIncorretos(string nome, string telefone, string email, int ddd, string mensagemEsperada)
+    {
+        // Arrange
+        var contato = new Contato
+        {
+            Id = 1,
+            Nome = nome,
+            Telefone = telefone,
+            Email = email,
+            Ddd = ddd
+        };
+
+        // Act
+        var validationResults = ValidateModel(contato);
+
+        // Assert
+        Assert.NotEmpty(validationResults);
+        Assert.Contains(validationResults, v => v.ErrorMessage == mensagemEsperada);
+    }
+
+    // Método auxiliar para realizar validações de modelo
+    private IList<ValidationResult> ValidateModel(object model)
+    {
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(model, serviceProvider: null, items: null);
+        Validator.TryValidateObject(model, validationContext, validationResults, validateAllProperties: true);
+        return validationResults;
+    }
+}
