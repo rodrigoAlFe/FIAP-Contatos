562d4f146d4263294aba5cc27ed132b269ebc39c
LCSRuas <lucasiruas@gmail.com>
2025-07-27 11:47:12 -0300

    feat: Consumer do RabbitMq ouvindo a fila contatos que o Producer publica

--- src/persistencia-api/Program.cs
+++ src/persistencia-api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using persistencia_api.Data;
+using persistencia_api.Services;
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<RabbitMqContatoConsumer>();
 
 var app = builder.Build();
 
