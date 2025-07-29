3a437ca6c5ad1d9e89d43ac986d3d38fe93ff834
carlosalbertofilho <carlosalberto_filho@outlook.com>
2025-07-28 23:58:46 -0300

    feat: Adiciona variável de ambiente ASPNETCORE_ENVIRONMENT para desenvolvimento e remove script de migrations

--- migrations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e  # Para o script caso algum comando falhe
-
-echo "Preparando pra roda as migrations.."
-dotnet tool install --global dotnet-ef
-export PATH="$PATH:/root/.dotnet/tools"
-
-echo "Executando migrations..."
-dotnet ef database update --project src/persistencia-api/persistencia-api.csproj --startup-project src/persistencia-api/persistencia-api.csproj --context AppDbContext
